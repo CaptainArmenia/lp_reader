e4c5b4ac6332081740573ce3ce8685491fa560df
Andy <agbaloian@gmail.com>
2023-09-19 22:16:59 -0300

    multy read

--- register.py
+++ register.py
@@ -70,26 +70,58 @@ for video_path in video_paths:
                     x, y, w, h = box
                     track = track_history[track_id]
 
-                    # Analyze LP only if it has not already been read
-                    if track_id not in license_plates:
-                        # Check if object has been relatively quiet for the last 15 frames and bounding box area is sufficiently large
-                        if len(track) > 15:
-                            # Get the last 15 points
-                            last_15_points = track[-15:]
-                            # Calculate the average distance between the last 15 points
-                            distances = np.linalg.norm(np.diff(last_15_points, axis=0), axis=1)
-                            average_distance = np.mean(distances)
+                    # Check if object has been relatively quiet for the last 15 frames and bounding box area is sufficiently large
+                    if len(track) > 15:
+                        # Get the last 15 points
+                        last_15_points = track[-15:]
+                        # Calculate the average distance between the last 15 points
+                        distances = np.linalg.norm(np.diff(last_15_points, axis=0), axis=1)
+                        average_distance = np.mean(distances)
+                        
+                        # If the average distance is less than 10 pixels and box area is sufficiently large, start a stop counter
+                        if average_distance < 3 and w * h > 3000:
+                            stop_counter[track_id] += 1
+                        else:
+                            stop_counter[track_id] = 0
                             
-                            # If the average distance is less than 10 pixels and box area is sufficiently large, start a stop counter
-                            if average_distance < 3 and stop_counter[track_id] < 30 and w * h > 3000:
-                                stop_counter[track_id] += 1
-                            else:
-                                stop_counter[track_id] = 0
-                                
-                        track.append((float(x), float(y)))  # x, y center point
-                        if len(track) > 30:  # retain 90 tracks for 90 frames
-                            track.pop(0)
+                    track.append((float(x), float(y)))  # x, y center point
 
+                    if len(track) > 30:  # retain 90 tracks for 90 frames
+                        track.pop(0)
+
+                    # If the stop counter reaches 30, read and save the license plate
+                    if stop_counter[track_id] > 29 and stop_counter[track_id] < 40:
+                        license_plate_crop = frame[int(y - h / 2):int(y + h / 2), int(x - w / 2):int(x + w / 2)]
+
+                        # check if crop is not blurry
+                        if cv2.Laplacian(license_plate_crop, cv2.CV_64F).var() < 300:
+                            stop_counter[track_id] = 0
+                            continue
+
+                        license_plate, lp_conf = reader.read(license_plate_crop)
+                        
+                        # Check license plate validity
+                        if len(license_plate) != 6 or license_plate[0].isdigit() or license_plate[1].isdigit() or not license_plate[4].isdigit() or not license_plate[5].isdigit():
+                            stop_counter[track_id] = 0
+                        # Update license plate if it is valid
+                        else:
+                            if track_id not in license_plates:
+                                license_plates[track_id] = license_plate
+                                license_plates_confidences[track_id] = lp_conf
+                            else:
+                                if lp_conf > license_plates_confidences[track_id]:
+                                    license_plates[track_id] = license_plate
+                                    license_plates_confidences[track_id] = lp_conf
+
+                            registered_license_plates = list(license_plates.values())
+
+                            # show the license plate crop, together with its bluriness and the license plate
+                            license_plate_crop_copy = license_plate_crop.copy()
+                            cv2.putText(license_plate_crop_copy, "{:.2f}".format(cv2.Laplacian(license_plate_crop, cv2.CV_64F).var()), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 0), 2)
+                            cv2.imshow("License Plate", license_plate_crop_copy)
+        
+                     # Analyze LP only if it has not already been read
+                    if track_id not in license_plates:
                         # For each pair of consecutive points, draw a line between them with a thickness proportional to the age of the point
                         points = np.hstack(track).astype(np.int32).reshape((-1, 1, 2))
 
@@ -103,39 +135,6 @@ for video_path in video_paths:
                             cv2.circle(overlay, (int(x), int(y)), int(100 * (1 - 1 / stop_counter[track_id]) ** 3), (0, 255, 0), -1)
                             cv2.addWeighted(overlay, 0.5, vis_frame, 0.5, 0, vis_frame)
 
-                        # If the stop counter reaches 30, read and save the license plate
-                        if stop_counter[track_id] > 29 and stop_counter[track_id] < 40:
-                            license_plate_crop = frame[int(y - h / 2):int(y + h / 2), int(x - w / 2):int(x + w / 2)]
-
-                            # check if crop is not blurry
-                            if cv2.Laplacian(license_plate_crop, cv2.CV_64F).var() < 300:
-                                stop_counter[track_id] = 0
-                                continue
-
-                            license_plate, lp_conf = reader.read(license_plate_crop)
-                            
-                            # Check license plate validity
-                            if len(license_plate) != 6 or license_plate[0].isdigit() or license_plate[1].isdigit() or not license_plate[4].isdigit() or not license_plate[5].isdigit():
-                                stop_counter[track_id] = 0
-                            # Update license plate if it is valid
-                            else:
-                                if track_id not in license_plates:
-                                    license_plates[track_id] = license_plate
-                                    license_plates_confidences[track_id] = lp_conf
-                                else:
-                                    if lp_conf > license_plates_confidences[track_id]:
-                                        license_plates[track_id] = license_plate
-                                        license_plates_confidences[track_id] = lp_conf
-
-                                registered_license_plates = list(license_plates.values())
-
-                                # show the license plate crop, together with its bluriness and the license plate
-                                license_plate_crop_copy = license_plate_crop.copy()
-                                cv2.putText(license_plate_crop_copy, "{:.2f}".format(cv2.Laplacian(license_plate_crop, cv2.CV_64F).var()), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 0), 2)
-                                cv2.imshow("License Plate", license_plate_crop_copy)
-            
-                            stop_counter[track_id] += 1
-
                     # If track has an associated license plate, draw it on the frame beneath the bounding box
                     else:
                         license_plate = license_plates[track_id]
@@ -144,7 +143,7 @@ for video_path in video_paths:
                         # Write the license plate below the bounding box
                         cv2.putText(vis_frame, license_plate, (int(x - w / 2), int(y + h / 2 + 30)), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 0), 2)
                         # put confidence below with two decimals
-                        cv2.putText(vis_frame, "{:.2f}".format(lp_conf), (int(x - w / 2), int(y + h / 2 + 60)), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 0), 2)
+                        cv2.putText(vis_frame, "{:.2f}".format(license_plates_confidences[track_id]), (int(x - w / 2), int(y + h / 2 + 60)), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 0), 2)
 
             # Write the number of vehicles on the bottom left corner
             #cv2.putText(frame, "Ingresos: {}".format(len(inside_vehicle_ids)), (10, frame.shape[0] - 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 200, 0), 2)    
